cf0e727ee3dc3049c57c644937fcd3e6aff474d5
AwaisAnwar01 <awaisanwaar7077@gmail.com>
2025-02-25 19:04:05 +0500

    coreected models and schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 from datetime import datetime
 from enum import Enum
+from backend.models import Difficulty
+from uuid import UUID
 
 
 
@@ -37,51 +39,42 @@ class Token(BaseModel):
 
 
 class PromptBase(BaseModel):
-    name: Optional[str] = None
-    content: Optional[str] = None
-    category: Optional[str] = None
+    # prompt_id : UUID
+    content: str
+    tech_stack: str
+    difficulty: Difficulty
 
 class PromptCreate(PromptBase):
     pass
 
 class PromptResponse(PromptBase):
-    prompt_id: int
-    created_at: datetime
-    updated_at: datetime
+    prompt_id: str
+    content: str
+    tech_stack: str
+    difficulty: str
+    created_at: str
+    updated_at: str
 
     class Config:
         orm_mode = True
         
 class Prompt(PromptBase):
-    prompt_id: int
-    created_at: datetime
-    updated_at: datetime
+    prompt_id: str
+    created_at: str
+    updated_at: str
 
     class Config:
         orm_mode = True
 
 class Difficulty(str, Enum):
-    EASY = "EASY"
-    MEDIUM = "MEDIUM"
-    HARD = "HARD"
+    EASY = "easy"
+    MEDIUM = "medium"
+    HARD = "hard"
 
-class TechStackPromptBase(BaseModel):
-    tech_stack: Optional[str] = None
-    difficulty: Difficulty = Difficulty.EASY
-    generated_prompt: str
+class PromptUpdate(BaseModel):
+       content: str
+       tech_stack: str
+       difficulty: Difficulty
 
-class TechStackPromptCreate(TechStackPromptBase):
-    pass
-
-class TechStackPrompt(TechStackPromptBase):
-    tech_prompt_id: str
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
-class TechStackPromptResponse(TechStackPromptBase):
-    tech_prompt_id: str
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
+class PromptDelete(BaseModel):
+    prompt_id: UUID
